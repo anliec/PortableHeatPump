37bd7ec5493373186b12792207fbdc2f95f1dffd
nicolas <sixnicolas@outlook.com>
2023-08-14 19:21:29 -0700

    first cool

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,11 @@ constexpr uint8_t PIN_TEMP_SENSOR_AIR = A1;
 constexpr uint8_t PIN_WATER_SENSOR_NO = A2;
 constexpr uint8_t PIN_WATER_SENSOR_NC = A3;
 
+constexpr float OFF_TEMP_THRESHOLD = 3.0;
+constexpr float STANDBY_TEMP_THRESHOLD = 1.0;
+constexpr float UNFROST_START_TEMP_THRESHOLD = 5.0;
+constexpr float UNFROST_STOP_TEMP_THRESHOLD = 10.0;
+
 struct InFanSpeed
 {
   enum Enum : char
@@ -38,6 +43,22 @@ struct OutFanSpeed
   };
 };
 
+struct ACState
+{
+  enum Enum : char
+  {
+    Off,
+    StandBy,
+    Cooling,
+    Unfrosting,
+  };
+};
+
+
+float target;
+ACState::Enum state;
+
+
 void SetInFanSpeed(InFanSpeed::Enum speed)
 {
   // Turn off everything to clean previous state
@@ -57,6 +78,8 @@ void SetInFanSpeed(InFanSpeed::Enum speed)
   case InFanSpeed::High:
     digitalWrite(PIN_IN_FAN_HIGH, HIGH);
     break;
+  case InFanSpeed::Off:
+    break;
   }
 }
 
@@ -74,6 +97,8 @@ void SetOutFanSpeed(OutFanSpeed::Enum speed)
     break;
   case OutFanSpeed::High:
     digitalWrite(PIN_OUT_FAN_HIGH, HIGH);
+    break;
+  case OutFanSpeed::Off:
     break;
   }
 }
@@ -105,19 +130,30 @@ bool IsWaterFull(bool& hasError)
   }
 }
 
-int ReadTemperatureDeciCelcius(uint8_t pin)
+float ReadTempAir()
 {
-  return analogRead(pin);
+  constexpr float valAt27 = 437.;
+  constexpr int valAt39 = 717.;
+  // pulldown resistor: 15k
+  // We don't need a wide range of temperature, let's assume lineare transition in that range
+  float in = analogRead(PIN_TEMP_SENSOR_AIR);
+  float temp = (in - valAt27) * 12.0 / (valAt39 - valAt27) + 27.0;
+  Serial.print("airTempRaw:  ");
+  Serial.println(in);
+  return temp;
 }
 
-int ReadTempAir()
+float ReadTempCooler()
 {
-  return ReadTemperatureDeciCelcius(PIN_TEMP_SENSOR_AIR);
-}
-
-int ReadTempCooler()
-{
-  return ReadTemperatureDeciCelcius(PIN_TEMP_SENSOR_COOLER);
+  constexpr int valAt27 = 444.;
+  constexpr int valAt39 = 717.;
+  // pulldown resistor: 15k
+  // We don't need a wide range of temperature, let's assume lineare transition in that range
+  float in = analogRead(PIN_TEMP_SENSOR_AIR);
+  float temp = (in - valAt27) * 12.0 / (valAt39 - valAt27) + 27.0;
+  Serial.print("coolerTempRaw: ");
+  Serial.println(in);
+  return temp;
 }
 
 void setup() 
@@ -133,6 +169,9 @@ void setup()
   digitalWrite(PIN_STEPER_3, LOW);
   digitalWrite(PIN_STEPER_4, LOW);
 
+  target = 27.0;
+  state = ACState::Off;
+
   Serial.begin(9600);
 }
 
@@ -141,8 +180,8 @@ void loop()
   bool isError;
   const bool isWaterFull = IsWaterFull(isError);
 
-  const int airTemp = ReadTempAir();
-  const int coolerTemp = ReadTempCooler();
+  const float airTemp = ReadTempAir();
+  const float coolerTemp = ReadTempCooler();
 
   Serial.print("isError:     ");
   Serial.println(isError);
@@ -152,6 +191,92 @@ void loop()
   Serial.println(airTemp);
   Serial.print("coolerTemp:  ");
   Serial.println(coolerTemp);
+  Serial.print("State:       ");
+  switch (state)
+  {
+  case ACState::Unfrosting:
+    Serial.println("Unfrosting");
+    break;
+  case ACState::Cooling:
+    Serial.println("Cooling");
+    break;
+  case ACState::StandBy:
+    Serial.println("StandBy");
+    break;
+  case ACState::Off:
+    Serial.println("Off");
+    break;
+  }
+
+
+  const float delta_temp = airTemp - target;
+  
+
+  switch (state)
+  {
+  case ACState::Unfrosting:
+    if(coolerTemp > UNFROST_STOP_TEMP_THRESHOLD)
+    {
+      if(delta_temp < -STANDBY_TEMP_THRESHOLD)
+      {
+        state = ACState::StandBy;
+      }
+      else
+      {
+        state = ACState::Cooling;
+      }
+    }
+  case ACState::Cooling:
+    if(delta_temp < -STANDBY_TEMP_THRESHOLD)
+    {
+      state = ACState::StandBy;
+    }
+    else if(coolerTemp < UNFROST_START_TEMP_THRESHOLD)
+    {
+      state = ACState::Unfrosting;
+    }
+    break;
+  case ACState::StandBy:
+    if(delta_temp > 0.0)
+    {
+      state = ACState::Cooling;
+    }
+    else if(delta_temp < -OFF_TEMP_THRESHOLD)
+    {
+      state = ACState::Off;
+    }
+    break;
+  case ACState::Off:
+    if(delta_temp > -OFF_TEMP_THRESHOLD + 0.5)
+    {
+      state = ACState::StandBy;
+    }
+    break;
+  }
+
+  switch (state)
+  {
+  case ACState::Unfrosting:
+    SetOutFanSpeed(OutFanSpeed::Off);
+    SetInFanSpeed(InFanSpeed::High);
+    SetCompressorStatus(false);
+    break;
+  case ACState::Cooling:
+    SetOutFanSpeed(OutFanSpeed::High);
+    SetInFanSpeed(InFanSpeed::High);
+    SetCompressorStatus(true);
+    break;
+  case ACState::StandBy:
+    SetOutFanSpeed(OutFanSpeed::Off);
+    SetInFanSpeed(InFanSpeed::Low);
+    SetCompressorStatus(false);
+    break;
+  case ACState::Off:
+    SetOutFanSpeed(OutFanSpeed::Off);
+    SetInFanSpeed(InFanSpeed::Off);
+    SetCompressorStatus(false);
+    break;
+  }
 
   delay(1000);
 }
